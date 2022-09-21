69c57a637cdfc8c7dc40129192615e7220bf1f35
Jin-Yanhong <820877998@qq.com>
2022-09-21 20:30:47 +0800

    fixed some eslint errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	// <React.StrictMode>
-	<Provider store={store}>
-		<Router basename="">
-			<App />
-		</Router>
-	</Provider>,
-	// </React.StrictMode>
+    // <React.StrictMode>
+    <Provider store={store}>
+        <Router basename="/Blog">
+            <App />
+        </Router>
+    </Provider>
+    // </React.StrictMode>
 );
