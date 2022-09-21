d9ab2fa76d5d985f72f0f7e4d7c9f3ebc83bef89
Jin-Yanhong <820877998@qq.com>
2022-09-21 21:06:38 +0800

    update .env PUBLIC_URL variable

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import { store } from './store';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
+// 如果环境变量文件设置了 `PUBLIC_URL` ,那么路由不需要设置 basename
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     // <React.StrictMode>
     <Provider store={store}>
-        <Router basename="/Blog">
+        <Router>
             <App />
         </Router>
     </Provider>
